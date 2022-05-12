11a4114753cab9c8d836bf3d5eb8afce83eec5e0
printf_cherif <64066992+Maximus203@users.noreply.github.com>
2022-05-12 11:05:13 +0000

    Improve

--- src/components/MyPlayer.js
+++ src/components/MyPlayer.js
@@ -1,24 +1,25 @@
-export default function MyPlayer(params) {
+export default function MyPlayer({ id, name, team, goal, assist }) {
   return (
     <div>
-      <h3 className="text-center alert-info text-info p-2 display-1 rounded">
-        Player {params.id}
+      <h3 className="text-center alert-info text-info m-2 display-1 rounded">
+        Player {id}
       </h3>
       <ul type="square">
-        <li className="alert-success text-success p-2 display-2 rounded">
-          Name : {params.name}
+        <li className="alert-success text-success m-2 display-2 rounded">
+          Name : {name}
         </li>
-        <li className="alert-dark text-dark p-2 display-2 rounded">
-          Team : {params.team}
+        <li className="alert-dark text-dark m-2 display-2 rounded">
+          Team : {team}
         </li>
-        <li className="alert-danger text-danger p-2 display-2 rounded">
-          Goal : {params.goal}
+        <li className="alert-danger text-danger m-2 display-2 rounded">
+          Goal : {goal}
         </li>
-        <li className="alert-danger text-danger p-2 display-2 rounded">
-          Assist : {params.assist}
+        <li className="alert-danger text-danger m-2 display-2 rounded">
+          Assist : {assist}
         </li>
-        <li className="bg-danger text-white p-2 display-2 rounded">
-          Trophees : {params.goal + params.assist}
+        <li className="bg-danger text-white m-2 display-2 rounded">
+          Trophees :
+          {goal + assist >= 70 ? "Give him its BO" : "Needs to work !"}
         </li>
       </ul>
     </div>
